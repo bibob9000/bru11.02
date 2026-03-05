95ed8f0b60623aa96361175f743d4064abdb9cc6
bibob9000 <120679649+bibob9000@users.noreply.github.com>
2026-03-05 12:43:25 +0300

    Update Utils.java

--- app/src/main/java/com/example/myapplication/utils/Utils.java
+++ app/src/main/java/com/example/myapplication/utils/Utils.java
@@ -2,13 +2,13 @@ package com.example.myapplication.utils;
 import com.example.myapplication.data.User;
 
 public class Utils {
-    public static final String APIKEY = "F4sNmyQBmvjEkRlbVRZhFw_Luq9ob2l";
+    public static final String APIKEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4cnV6YmZxaW52b2txendnaHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk1NDQxOTcsImV4cCI6MjA4NTEyMDE5N30.j11KIVQwLTGc9N5c5ZUSnzGoeYeWmR_Gr44jmXVug78";
 
     public static final String CONTENT_TYPE = "application/json";
 
     public static User user = new User("", "");
 
-    public static final String BASE_URL = "https://ixruzbfqinvokqzwghrp.supabase.co";
+    public static final String BASE_URL = "https://ixruzbfqinvokqzwghrp.supabase.co/auth/v1/";
 
     public static final String GRANT_TYPE = "password";
 
